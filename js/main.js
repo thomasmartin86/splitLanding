53b79f9921951ced8693280447c63bcf61bad860
thomasmartin86 <thomas.martin1126@gmail.com>
2018-09-25 14:22:25 -0400

    Updates

---
+++ js/main.js
@@ -0,0 +1,19 @@
+/* remove and add classes */
+const left = document.querySelector('.left');
+const right = document.querySelector('.right');
+const container = document.querySelector('.container');
+
+/* add event listeners for mouse enter and leave */
+left.addEventListener('mouseenter', () => {
+  container.classList.add('hover-left');
+});
+left.addEventListener('mouseleave', () => {
+  container.classList.remove('hover-left');
+});
+
+right.addEventListener('mouseenter', () => {
+  container.classList.add('hover-right');
+});
+right.addEventListener('mouseleave', () => {
+  container.classList.remove('hover-right');
+});
